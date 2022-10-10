77c57e6ebf8ec6bdf213442e1895e747d2afe4e1
Alpaca Uniix <supatsara_wan@cmu.ac.th>
2022-10-10 13:19:00 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React UNIIX
+          Learn React UALPACAUNIIX
         </a>
       </header>
     </div>
