522c740287b701c30ce0b00bdc506682b6d2d603
Alpaca Uniix <supatsara_wan@cmu.ac.th>
2022-10-10 11:44:50 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello, UNIIX
+          Learn React UNIIX
         </a>
       </header>
     </div>
